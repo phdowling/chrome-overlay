e37f4587395b6a5b0dbcf6ab64544cf8836c4a10
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-09-11 01:01:18 +0200

    correctly handle tab selection for adding and removing overlays

--- background.js
+++ background.js
@@ -3,23 +3,25 @@
 // found in the LICENSE file.
 
 // Called when the user clicks on the browser action.
-overlayed = false;
+overlayed = {};
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or  host permissions needed!
-  console.log("Clicked!");
-  if(!overlayed){
-    console.log('Adding overlay to page!');
-    chrome.tabs.insertCSS(null, {file: "style/style.css"}, function(){
-      chrome.tabs.executeScript(null, { file: "jquery/jquery-2.1.4.min.js" }, function() {
-        chrome.tabs.executeScript(null, { file: "overlay_logic/content_script.js" });
-      });
-    });
-    console.log("Overlay loaded.");
-    overlayed = true;
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    console.log("Clicked!");
+    if(!(overlayed[tabs[0].id] || false)){
+      console.log('Adding overlay to page!');
 
-  }else{
-    console.log("Telling overlay to close.");
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      chrome.tabs.insertCSS(tabs[0].id, {file: "style/style.css"}, function(){
+        chrome.tabs.executeScript(tabs[0].id, { file: "jquery/jquery-2.1.4.min.js" }, function() {
+          chrome.tabs.executeScript(tabs[0].id, { file: "overlay_logic/content_script.js" });
+        });
+      });
+
+      console.log("Overlay loaded.");
+      overlayed[tabs[0].id] = true;
+
+    }else{
+      console.log("Telling overlay to close.");
       chrome.tabs.sendMessage(
         tabs[0].id,
         {message: "remove overlay"},
@@ -27,8 +29,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
           console.log(response.message);
         }
       );
-    });
-    overlayed = false;
-  }
-
+      overlayed[tabs[0].id] = false;
+    }
+  });
 });
