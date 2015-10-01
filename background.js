b4c8429af583811251d771b729324ccfe6dd591e
Philipp Dowling <philipp.dowling@quartett-mobile.de>
2015-10-01 13:03:43 +0200

    per-tab overlays working cleanly

--- background.js
+++ background.js
@@ -1,35 +1,87 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Called when the user clicks on the browser action.
+initialized = {};
 overlayed = {};
+
+// When the action icon is clicked, we need to show or hide the overlay
 chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or  host permissions needed!
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    console.log("Clicked!");
-    if(!(overlayed[tabs[0].id] || false)){
-      console.log('Adding overlay to page!');
-
-      chrome.tabs.insertCSS(tabs[0].id, {file: "style/style.css"}, function(){
-        chrome.tabs.executeScript(tabs[0].id, { file: "jquery/jquery-2.1.4.min.js" }, function() {
-          chrome.tabs.executeScript(tabs[0].id, { file: "overlay_logic/content_script.js" });
-        });
-      });
-
-      console.log("Overlay loaded.");
-      overlayed[tabs[0].id] = true;
-
+    tabId = tabs[0].id;
+    if(!initialized[tabId]){
+      // This is the first time the icon was clicked for the current tab, initialize content script
+      initializeOverlay(tabId);
     }else{
-      console.log("Telling overlay to close.");
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        {message: "remove overlay"},
-        function(response) {
-          console.log(response.message);
-        }
-      );
-      overlayed[tabs[0].id] = false;
+      // Content script is running, we just need to tell it to show or hide the overlay
+      toggleOverlayVisibility(tabId);
     }
   });
 });
+
+// when the URL changes or the page is refreshed, both initialized and overlayed need to change to false for that tab
+chrome.webNavigation.onCommitted.addListener(function(details){
+  if(details.frameId == 0){ // only reset if the nav is tab-level
+    resetTabOverlayState(details.tabId);
+  }
+});
+
+function initializeOverlay(tabId){
+  console.log('Adding first overlay to page!');
+  chrome.tabs.insertCSS(tabId, {file: "style/style.css"}, function(){
+    executeScripts(tabId,[
+      { file: "jquery/jquery-2.1.4.min.js" },
+      { file: "overlay_logic/content_script.js" }
+    ], function(){
+      openOverlay(tabId);
+      console.log("Overlay loaded and opened.");
+      initialized[tabId] = true;
+      overlayed[tabId] = true;
+    });
+  });
+}
+
+function resetTabOverlayState(tabId){
+  console.log("Setting tab " + details.tabId + " to uninitialized.")
+  initialized[tabId] = false;
+  overlayed[tabId] = false;
+}
+
+function toggleOverlayVisibility(tabId){
+  if(overlayed[tabId]){
+    closeOverlay(tabId);
+  }else{
+    openOverlay(tabId);
+  }
+}
+
+function openOverlay(tabId) {
+  sendMessageToTab(tabId, "open overlay")
+  overlayed[tabId] = true;
+}
+
+function closeOverlay(tabId) {
+  sendMessageToTab(tabId, "close overlay")
+  overlayed[tabId] = false;
+}
+
+function sendMessageToTab(tabId, message_){
+  console.log("Sending message '" + message_ + "' to tab " + tabId);
+  chrome.tabs.sendMessage(
+    tabId,
+    {message: message_},
+    function(response) {
+      console.log("Response: '" + response.message + "'");
+    }
+  );
+}
+
+function executeScripts(tabId, injectDetailsArray, callback){
+    function createCallback(tabId, injectDetails, innerCallback) {
+      return function () {
+        chrome.tabs.executeScript(tabId, injectDetails, innerCallback);
+      };
+    }
+    for (var i = injectDetailsArray.length - 1; i >= 0; --i){
+      callback = createCallback(tabId, injectDetailsArray[i], callback);
+    }
+    if (callback !== null){
+      callback();   // execute outermost function
+    }
+}
--- overlay_logic/content_script.js
+++ overlay_logic/content_script.js
@@ -1,28 +1,30 @@
-// document.body.style.backgroundColor="red"
-console.log("Hello world!");
+console.log("Content script executing.");
+
+overlayDiv = $('<div id="overlay"/>');
+/* TODO: asynchronously add our content to overlay div element
+  The idea is that the content is loaded only the first time the extension icon is clicked.
+  Beyond that, content is not changed (except maybe refreshing), but the overlay is simply hidden or re-added.
+*/
 
 function displayOverlay() {
   console.log("Adding overlay to page.")
-  $('<div id="overlay"/>').appendTo($("body").css("position", "relative"));
-  // TODO: add business logic to overlay element
-}
-
+  $("body").prepend(overlayDiv);
+};
 
 function removeOverlay(){
   console.log("Removing overlay!");
-  $("#overlay").remove();
-}
-
+  $("#overlay").detach();
+};
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.message == "remove overlay"){
+    console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+    if (request.message == "close overlay"){
       removeOverlay();
       sendResponse({message: "goodbye!"});
+    } else if (request.message == "open overlay") {
+      displayOverlay();
+      sendResponse({message: "hello!"});
     }
-  });
-
-displayOverlay();
+  }
+);
